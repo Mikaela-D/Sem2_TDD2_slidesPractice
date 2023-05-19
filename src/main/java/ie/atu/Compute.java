6e05946809b6b1a4670a5840f37cd3e8545bf5b6
Mikaela-D <G00401392@atu.ie>
2023-05-19 21:07:13 +0100

    Added calculation code in the Computer class

--- src/main/java/ie/atu/Compute.java
+++ src/main/java/ie/atu/Compute.java
@@ -2,6 +2,14 @@ package ie.atu;
 
 public class Compute {
     public float calc(float arg1, float arg2) {
+        float x = arg1;
+        float y = arg2;
 
+        if (x != 0) {
+            y += 10;
+        }
+        y = y / x;
+        System.out.println(x + " " + y);
+        return y;
     }
 }
