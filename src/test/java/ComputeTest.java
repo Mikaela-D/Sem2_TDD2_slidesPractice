2cfa2312ead5ef2afceeb726e582547d89c8f695
Mikaela-D <G00401392@atu.ie>
2023-05-19 21:15:23 +0100

    Added testing code in the ComputeTest class

--- src/test/java/ComputeTest.java
+++ src/test/java/ComputeTest.java
@@ -1,2 +1,13 @@
+import ie.atu.Compute;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ComputeTest {
+
+    @Test
+    void calc() {
+        Compute myCompute = new Compute();
+        assertEquals(12, myCompute.calc(1, 2));
+    }
 }
